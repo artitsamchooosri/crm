b95f0bf96934610eae50830243f80283a7d0215a
artitsamchooosri <33492904+artitsamchooosri@users.noreply.github.com>
2022-10-19 23:22:27 +0700

    Update initialize.php

--- initialize.php
+++ initialize.php
@@ -4,7 +4,7 @@ foreach($dev_data as $k => $v){
 if(!defined('dev_'.$k)) define('dev_'.$k,$v);
 }
 if(!defined('dev_json_data')) define('dev_json_data',json_encode($dev_data));
-if(!defined('base_url')) define('base_url','http://localhost/crm/');
+if(!defined('base_url')) define('base_url','http://app1.thanaporn.shop/crm/');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 if(!defined('dev_data')) define('dev_data',$dev_data);
 if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
